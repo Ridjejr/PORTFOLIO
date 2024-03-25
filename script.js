27960ca5e560f8a1fe5486c11dc9014f71496b5b
esdras missilou <esdrasmissilou@gmail.com>
2024-03-25 18:20:57 +0100

    Version 1.2

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function toggleMenu() {
-    const menu =  document.querySelector(".menu-links");
-    const icon =  document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+  // sert à ouvrir et fermer le menu
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  AOS.init({
+    duration: 800, // Durée de l'animation en millisecondes
+    offset: 200, // Décalage par rapport au défilement avant que l'élément ne devienne visible
+  });
+});
